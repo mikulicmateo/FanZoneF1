6f8454af0e9abf5e3b4a12390b037381c7bab9ca
Mateo Mikulic <mikulic.dev@gmail.com>
2021-12-19 12:04:27 +0100

    Fixed bugs.

--- src/main/java/hr/riteh/fanzonef1/dto/response/UserStandingsResponseDto.java
+++ src/main/java/hr/riteh/fanzonef1/dto/response/UserStandingsResponseDto.java
@@ -1,5 +1,12 @@
 package hr.riteh.fanzonef1.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserStandingsResponseDto {
     private String username;
     private String points;
